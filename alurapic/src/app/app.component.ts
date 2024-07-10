9a09d486ff3115eee5993d71d96a598df60b4703
Marcos Paulo <marcos.poo@gmail.com>
2024-07-09 23:30:25 -0300

    Aula 2 - Criando o primeiro componente
    Nesta aula:
    - Como o arquivo bootstrap.css pode ser adicionado ao processo de build do Angular CLI.
    - Criação de um novo componente
    - A importância de declarar o componente em um módulo
    - Como passar dados para o componente através das inbound properties
    - Criação de um módulo e boas práticas
    - A diretiva *ngFor

--- alurapic/src/app/app.component.ts
+++ alurapic/src/app/app.component.ts
@@ -6,7 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Rambo Vs Matrix';
-  description='RamboVsMatrix';
-  url='https://scontent.ffor41-1.fna.fbcdn.net/v/t39.30808-6/296117101_596229748533264_741822184633682206_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeG6WQjAaoGwcp6TPdltzgr0t2n7FTaQQci3afsVNpBByOBUrXm3DC7_hBEe5jpoyQZ2gF9uZED5z_G7C9-SprdJ&_nc_ohc=0LmmLhIy13EQ7kNvgEyW1LD&_nc_ht=scontent.ffor41-1.fna&oh=00_AYA0hItmwGHwvJ50Ib9SOoO2KMLlhWYGDO6k35d7GBRUhQ&oe=66939B38'
+
+  photos = [
+    {
+      url: 'https://musicart.xboxlive.com/7/cf715100-0000-0000-0000-000000000002/504/image.jpg?w=1920&h=1080',
+      description: 'Jonh Rambo'
+    },
+    {
+      url:'https://scontent.ffor41-1.fna.fbcdn.net/v/t39.30808-6/296117101_596229748533264_741822184633682206_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeG6WQjAaoGwcp6TPdltzgr0t2n7FTaQQci3afsVNpBByOBUrXm3DC7_hBEe5jpoyQZ2gF9uZED5z_G7C9-SprdJ&_nc_ohc=0LmmLhIy13EQ7kNvgEyW1LD&_nc_ht=scontent.ffor41-1.fna&oh=00_AYA0hItmwGHwvJ50Ib9SOoO2KMLlhWYGDO6k35d7GBRUhQ&oe=66939B38',
+      description: 'Rambo Vs Matrix'
+    },
+    {
+      url: 'https://m.media-amazon.com/images/I/71A9FHT05GL._AC_UF894,1000_QL80_.gif',
+      description: 'Jonh Matrix'
+    }
+  ];
 }
